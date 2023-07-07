9dfee9421d76ef336ce5aa70e86ea64224dd0844
jl-do <doerzbacherjiaine@gmaill.com>
2023-07-07 16:03:22 -0300

    Resource

---
+++ src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -0,0 +1,96 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String edition;
+    private Instant startMoment;
+    private Instant endMoment;
+
+    @ManyToOne // Offer tem um Course (muitos para um)   // @ManyToOne: muitas ofertas para um curso
+    @JoinColumn(name = "course_id") // chave estrangeira na tabela tb_offer (course_id) vai dizer qual é o curso da oferta
+    private Course course;
+
+    @OneToMany(mappedBy = "offer") // Offer tem muitos Resources (um para muitos) // @OneToMany: uma oferta para muitos recursos
+    private List<Resource> resources = new ArrayList<>();
+
+    public Offer() {
+    }
+
+    public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course) {
+        this.id = id;
+        this.edition = edition;
+        this.startMoment = startMoment;
+        this.endMoment = endMoment;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public Instant getStartMoment() {
+        return startMoment;
+    }
+
+    public void setStartMoment(Instant startMoment) {
+        this.startMoment = startMoment;
+    }
+
+    public Instant getEndMoment() {
+        return endMoment;
+    }
+
+    public void setEndMoment(Instant endMoment) {
+        this.endMoment = endMoment;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Offer)) return false;
+        Offer offer = (Offer) o;
+        return Objects.equals(id, offer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
